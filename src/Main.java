852855a323c456e866c3f6d4f021dfcc7d5ec436
admin <vmahadevan5566@gmail.com>
2023-07-21 14:59:16 +0530

    mathavan

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        System.out.println(cars[0]);
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda","cars"};
+        System.out.println(cars[4]);
     }
 }
